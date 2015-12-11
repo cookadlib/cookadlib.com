a7ec3a3d3cbcd9634f8c86d39408e97a9abe6e49
Karl Podger <karl.podger@primeordinal.com>
2015-12-11 11:48:26 +0000

    Add deployment tasks to task runner

---
+++ gulp_tasks/tag.babel.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import git from 'gulp-git';
+import gulp from 'gulp';
+
+import config from './_config.babel.js';
+
+let version = 'v' + config.version;
+let message = `Release ${version}`;
+
+gulp.task('tag', ['bump'], () => {
+
+  // git.tag(version, message, {
+  //   args: 'signed',
+  //   cwd: '../'
+  // }, (err) => {
+  //   if (err) throw err;
+  // });
+  //
+  // git.push('origin', 'master', {
+  //   args: '--tags',
+  //   cwd: '../'
+  // }, function (err) {
+  //   if (err) throw err;
+  // });
+
+  git.exec({
+    args: `release ${version}`
+    // cwd: '../'
+  }, function (err) {
+    if (err) throw err;
+  });
+
+});
