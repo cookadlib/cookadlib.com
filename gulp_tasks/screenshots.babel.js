eb7762979fd22f61fa329714eb265eb3a2a020dc
Karl Podger <karl@wednnesdayagency.com>
2016-04-06 12:54:32 +0100

    Use Pageres to screenshot homepage

--- gulp_tasks/screenshots.babel.js
+++ gulp_tasks/screenshots.babel.js
@@ -10,70 +10,21 @@ import remember from 'gulp-remember';
 import size from 'gulp-size';
 // import webshot from 'gulp-webshot';
 
-import config from './_config.babel.js';
+import {config, browserSync} from './_config.babel.js';
 import reportError from './_report-error.babel.js';
 
 let sourceFiles = config.files.source.markup;
 
-// let screenSizes = config.instance.webshot.screenSizes;
-
-// let screenshotTasks = Object.keys(screenSizes).map(function(currentValue) {
-//   return `screenshots-${currentValue}`;
-// });
-
-// screenshotTasks.forEach(function(screenSize) {
-//
-//   gulp.task(screenSize, () => {
-//     return gulp.src(sourceFiles)
-//       .pipe(plumber({
-//         errorHandler: reportError
-//       }))
-//       .pipe(debug({
-//         title: 'screenshots:'
-//       }))
-//       .pipe(webshot({
-//         // phantomPath: phantomjsPath,
-//         dest: config.path.destination.screenshots,
-//         root: config.path.source.base,
-//         screenSize: screenSizes[screenSize],
-//         shotSize: {
-//           height: 'window',
-//           width: 'window'
-//         }
-//       }))
-//       .pipe(plumber.stop())
-//       .on('error', reportError);
-//   });
-//
-// });
-
-// gulp.task('screenshots', screenshotTasks);
-
-let pageres = new Pageres({
-  crop: true,
-  delay: 2,
-  scale: 0.1953125
-});
-
-gulp.task('screenshots', () => {
-  return gulp.src(sourceFiles)
-    .pipe(plumber({
-      errorHandler: reportError
-    }))
-    .pipe(debug({
-      title: 'screenshots:'
-    }))
-    // .pipe(pageres
-    // .src(file, ['1024x768'], {
-    //   filename: name
-    // })
-    // .dest(config.path.destination.screenshots + name)
-    // .run(function(err) {
-    //   console.log(err || 'done');
-    // }))
-    .pipe(size({title: 'screenshots'}))
-    .pipe(plumber.stop())
-    .on('error', reportError);
+gulp.task('screenshots', ['browser-sync'], () => {
+  return new Pageres({
+      delay: 2
+    })
+    .src(`http://localhost:${config.instance.browsersync.port}`, config.screenshots.sizes, {
+      crop: true
+    })
+    // .src('todomvc.com', ['1280x1024', '1920x1080'])
+    .dest(config.path.destination.screenshots)
+    .run();
 });
 
 gulp.task('screenshots:watch', () => {
