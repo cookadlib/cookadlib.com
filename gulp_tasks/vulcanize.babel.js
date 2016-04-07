97b921fd5713537984469fcc36306ddc7633dceb
Karl Podger <karl@wednnesdayagency.com>
2016-04-07 09:55:09 +0100

    Fix Vulcanize task to watch all elements

--- gulp_tasks/vulcanize.babel.js
+++ gulp_tasks/vulcanize.babel.js
@@ -1,25 +1,40 @@
 'use strict';
 
 import cache from 'gulp-cached';
+import debug from 'gulp-debug';
 import gulp from 'gulp';
+import plumber from 'gulp-plumber';
 import remember from 'gulp-remember';
 import size from 'gulp-size';
 import vulcanize from 'gulp-vulcanize';
 
 import {config, browserSync} from './_config.babel.js';
+import reportError from './_report-error.babel.js';
 
-let sourceFiles = `${config.path.source.elements}/elements.html`;
+let sourceFiles = [
+  `${config.path.source.elements}/elements.html`
+];
 sourceFiles = sourceFiles.concat(config.files.source.elements);
 
 gulp.task('vulcanize', () => {
   return gulp.src(`${config.path.source.elements}/elements.html`)
+    .pipe(plumber({
+      errorHandler: reportError
+    }))
+    .pipe(cache('vulcanize')) // only pass through changed files
+    .pipe(debug({
+      title: 'vulcanize:'
+    }))
     .pipe(vulcanize({
       stripComments: true,
       inlineCss: true,
       inlineScripts: true
     }))
     .pipe(gulp.dest(config.path.destination.elements))
-    .pipe(size({title: 'vulcanize'}));
+    .pipe(remember('vulcanize')) // add back all files to the stream
+    .pipe(size({title: 'vulcanize'}))
+    .pipe(plumber.stop())
+    .on('error', reportError);
 });
 
 gulp.task('vulcanize:watch', ['browser-sync'], () => {
