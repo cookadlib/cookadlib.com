730a77445e7b26b14d96cf8782d03e53655889ad
Karl Podger <karl@wednnesdayagency.com>
2016-04-07 10:07:24 +0100

    Copy bower_components to www in bower gulp task

--- gulp_tasks/bower.babel.js
+++ gulp_tasks/bower.babel.js
@@ -5,7 +5,7 @@ import cache from 'gulp-cached';
 import debug from 'gulp-debug';
 import gulp from 'gulp';
 import remember from 'gulp-remember';
-import rsync from 'gulp-rsync';
+// import rsync from 'gulp-rsync';
 
 import {config, browserSync} from './_config.babel.js';
 import reportError from './_report-error.babel.js';
@@ -13,16 +13,31 @@ import reportError from './_report-error.babel.js';
 let sourceFiles = config.files.source.bowerConfiguration;
 
 gulp.task('bower', () => {
-  return bower({
+  bower({
+    cmd: 'install'
+  })
+  .pipe(debug({
+    title: 'bower install:'
+  }))
+  // .pipe(rsync({
+  //   root: config.path.root,
+  //   destination: config.path.destination.bowerComponents
+  // }))
+  .pipe(gulp.dest(config.path.destination.bowerComponents))
+  .on('end', browserSync.reload)
+  .on('error', reportError);
+
+  bower({
     cmd: 'update'
   })
   .pipe(debug({
-    title: 'bower:'
+    title: 'bower update:'
   }))
-  .pipe(rsync({
-    root: config.path.source.bowerComponents,
-    destination: config.path.destination.bowerComponents
-  }))
+  // .pipe(rsync({
+  //   root: config.path.root,
+  //   destination: config.path.destination.bowerComponents
+  // }))
+  .pipe(gulp.dest(config.path.destination.bowerComponents))
   .on('end', browserSync.reload)
   .on('error', reportError);
 });
