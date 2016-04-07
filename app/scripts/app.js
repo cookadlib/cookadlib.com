ca56de13f120d5098046715ffabb859b957950d9
Karl Podger <karl@wednnesdayagency.com>
2016-04-07 10:01:46 +0100

    Do not use Polymer async bundle approach

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -40,9 +40,10 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
     // imports are loaded and elements have been registered
-    let splash = document.getElementById('splash');
-    splash.addEventListener('transitionend', splash.remove);
-    document.body.classList.remove('loading');
+
+    // let splash = document.getElementById('splash');
+    // splash.addEventListener('transitionend', splash.remove);
+    // document.body.classList.remove('loading');
   });
 
   // Main area's paper-scroll-header-panel custom condensing transformation of
