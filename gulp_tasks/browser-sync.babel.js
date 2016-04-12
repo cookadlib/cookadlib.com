9e174e454d9cecf80f543050bcfbf4982896c75e
Karl Podger <karl@wednnesdayagency.com>
2016-04-12 12:26:45 +0100

    Output browser-sync status via callback

--- gulp_tasks/browser-sync.babel.js
+++ gulp_tasks/browser-sync.babel.js
@@ -18,27 +18,28 @@ sourceFiles = sourceFiles.concat(config.files.source.markupIgnored.map(function(
   return '!' + path;
 }));
 
-if(config.instance.browsersync.server) {
-  config.instance.browsersync.server.middleware = [
-    historyApiFallback()
-  ];
-  console.info('Browsersync server set via config.instance.browsersync.server, not using config.instance.browsersync.proxy');
-} else if(config.instance.browsersync.proxy) {
-  console.info('Browsersync server set via config.instance.browsersync.proxy, not using config.instance.browsersync.server');
-}
+function onBrowsersyncInit() {
 
-// function onBrowsersyncInit() {
-//   console.info(
-//     'To connect from an iOS device:\n',
-//     util.colors.magenta(
-//       'Go to Settings > Wi-Fi\n',
-//       'Tap the information icon next to the Wi-Fi network name that the device is connected to.\n',
-//       'From the Wi-Fi network settings page, set HTTP Proxy to "Auto"\n',
-//       `Enter "${config.browsersync.proxy.protocol}://${browserSync.instance.utils.devIp}" into the "URL" field\n`,
-//       `Enter ${browserSync.instance.options.get('urls').get('external')} into the browser address bar\n`
-//     )
-//   );
-// }
+  if(config.instance.browsersync.server) {
+    config.instance.browsersync.server.middleware = [
+      historyApiFallback()
+    ];
+    console.info('Browsersync server set via config.instance.browsersync.server, not using config.instance.browsersync.proxy');
+  } else if(config.instance.browsersync.proxy) {
+    console.info('Browsersync server set via config.instance.browsersync.proxy, not using config.instance.browsersync.server');
+  }
+
+  // console.info(
+  //   'To connect from an iOS device:\n',
+  //   util.colors.magenta(
+  //     'Go to Settings > Wi-Fi\n',
+  //     'Tap the information icon next to the Wi-Fi network name that the device is connected to.\n',
+  //     'From the Wi-Fi network settings page, set HTTP Proxy to "Auto"\n',
+  //     `Enter "${config.browsersync.proxy.protocol}://${browserSync.instance.utils.devIp}" into the "URL" field\n`,
+  //     `Enter ${browserSync.instance.options.get('urls').get('external')} into the browser address bar\n`
+  //   )
+  // );
+}
 
 gulp.task('browser-sync', [
     // 'build' //no need to build every time that gulp is run; if you want to build then just type "gulp build" manually
@@ -47,7 +48,6 @@ gulp.task('browser-sync', [
     //   files: sourceFiles
     // });
 
-    // browserSync.init(config.instance.browsersync, onBrowsersyncInit);
-    browserSync.init(config.instance.browsersync);
+    browserSync.init(config.instance.browsersync, onBrowsersyncInit);
   }
 );
